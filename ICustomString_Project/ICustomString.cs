2ea2526e8d58d3fd7667566e258ead5b8e164927
jcanderson195 <jcanderson195@gmail.com>
2016-04-20 11:07:15 -0500

    Created classes and added interface functions

---
+++ ICustomString_Project/ICustomString.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICustomString_Project
+{
+    interface ICustomString
+    {
+
+        string ToString();
+
+        void Insert(string stringToInsert);
+
+        void Remove(int startIndex, int numCharsToRemove);
+
+        int Length();
+        
+
+    }
+}
